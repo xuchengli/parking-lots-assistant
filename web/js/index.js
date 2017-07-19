b94c7aa91fdaab326dae1db61542b9592227651a
lixucheng <lixucheng@aliyun.com>
2017-07-19 18:02:17 +0800

    npm install customization uikit, replace uikit to use custom icons

--- web/js/index.js
+++ web/js/index.js
@@ -2,7 +2,7 @@
  * Created by lixuc on 2017/7/16.
  */
 import "../css/style.less";
-import UIkit from "uikit";
-import Icons from "uikit/dist/js/uikit-icons";
+import UIkit from "uikit-customization";
+import Icons from "uikit-customization/dist/js/uikit-icons";
 
 UIkit.use(Icons);
